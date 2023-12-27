76ab9450c919207707bfcfe679cb3925a74224dd
unknown <zyam8441@gmail.com>
2023-12-27 17:05:47 +0900

    redux routes 초기설정

---
+++ front-end/src/pages/MainPage.tsx
@@ -0,0 +1,5 @@
+export default function MainPage(){
+    return(<>
+    <div>여기는 메인페이지</div>
+    </>)
+}
---
+++ front-end/src/store/index.tsx
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+
+const store = configureStore({
+    reducer: {
+       // nickname: 리듀서이름(추가할때 경로 적어야함)
+       
+      },
+});
+
+
+
+export default store;
