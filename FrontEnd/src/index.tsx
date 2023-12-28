0f91e89335d2b38c74eb8a3f9eacb6e6349ae8c6
ErrorMonkey <puts.hello.world.return0@gmail.com>
2023-12-28 15:38:44 +0900

    FrontEnd 폴더 프리티어 정리

--- FrontEnd/src/index.tsx
+++ FrontEnd/src/index.tsx
@@ -5,17 +5,12 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   //<React.StrictMode>
   <Provider store={store}>
-     <App />
+    <App />
   </Provider>
-   
- 
-    
+
   //</React.StrictMode>
 );
--- FrontEnd/src/pages/MainPage.tsx
+++ FrontEnd/src/pages/MainPage.tsx
@@ -1,5 +1,7 @@
-export default function MainPage(){
-    return(<>
-    <div>여기는 메인페이지</div>
-    </>)
+export default function MainPage() {
+  return (
+    <>
+      <div>여기는 메인페이지</div>
+    </>
+  );
 }
