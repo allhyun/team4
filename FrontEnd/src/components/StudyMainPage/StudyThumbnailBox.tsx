cec36cc1002938d4075f17314ef307199abb957c
zyam1 <zyam8441@gmail.com>
2023-12-28 21:25:53 +0900

    feat:페이지 생성

---
+++ FrontEnd/src/components/StudyMainPage/StudyThumbnailBox.tsx
@@ -0,0 +1,16 @@
+//--------들어가야할 요소
+//스터디 이름, 설명,
+//포지션별로 몇명구하고있는지
+//누르면 상세페이지로 이동
+
+//여기서 데이터를 받아온다음에 mab사용해서 정보 표시
+
+const StudyThumbnailBox = () => {
+  return (
+    <>
+      <div className="thum">스터디모집 섬네일</div>
+    </>
+  );
+};
+
+export default StudyThumbnailBox;
