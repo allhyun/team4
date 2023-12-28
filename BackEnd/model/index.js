77fba8d2592dad1ea86fbf4fa5a03a1d1361a1db
allhyun <da0956@naver.com>
2023-12-28 18:42:24 +0900

    model relation 파일 분리 및 controller study 수정

--- BackEnd/model/index.js
+++ BackEnd/model/index.js
@@ -1,6 +1,6 @@
 
 const Sequelize = require('sequelize');
-// const config = require("../config/config.json")["development"];
+const config = require("../config/config.json")["development"];
 
 
 const db = {};
@@ -20,78 +20,18 @@ db.Board = require('./Board')(sequelize, Sequelize);
 db.Usedgoods = require('./Usedgoods')(sequelize, Sequelize);
 db.Chattingroom = require('./Chattingroom')(sequelize, Sequelize);
 db.Chatmessage = require('./Chatmessage')(sequelize, Sequelize);
+db.User = require('./User')(sequelize, Sequelize);
+db.Study = require('./Study')(sequelize, Sequelize);
+db.Board = require('./Board')(sequelize, Sequelize);
+db.Usedgoods = require('./Usedgoods')(sequelize, Sequelize);
+db.Chattingroom = require('./Chattingroom')(sequelize, Sequelize);
+db.Chatmessage = require('./Chatmessage')(sequelize, Sequelize);
+db.Volunteer = require('./Volunteer')(sequelize, Sequelize);
+db.Heart = require('./Heart')(sequelize, Sequelize);
 
-// // User가 Board랑 조인
-// db.User.hasMany(db.Board, {
-//   foreignKey: "U_IDX",
-// });
-// db.Board.belongsTo(db.User, {
-//   onDelete: "cascade",
-//   foreignKey: "U_IDX",
-// });
 
-
-// User : Usedgoods
-db.User.hasMany(db.Usedgoods, {
-  foreignKey: 'u_idx',
-});
-db.Usedgoods.belongsTo(db.User, {
-  onDelete: 'cascade',
-  foreignKey: 'u_idx',
-});
-
-// User : Study
-db.User.hasMany(db.Study, {
-  foreignKey: 'u_idx',
-});
-db.Study.belongsTo(db.User, {
-  onDelete: 'cascade',
-  foreignKey: 'u_idx',
-});
-
-// User : Volunteer
-db.User.hasMany(db.Volunteer, {
-  foreignKey: 'u_idx',
-});
-db.Volunteer.belongsTo(db.User, {
-  onDelete: 'cascade',
-  foreignKey: 'u_idx',
-});
-// Study : volunteer
-db.Study.hasMany(db.Volunteer, {
-  foreignKey: 'st_idx',
-});
-db.Volunteer.belongsTo(db.Study, {
-  onDelete: 'cascade',
-  foreignKey: 'st_idx',
-});
-
-// User : Chattingroom
-db.User.hasMany(db.Chattingroom, {
-  foreignKey: 'u_idx',
-});
-db.Chattingroom.belongsTo(db.User, {
-  onDelete: 'cascade',
-  foreignKey: 'u_idx',
-});
-
-// User : Chatmessage
-db.User.hasMany(db.Chatmessage, {
-  foreignKey: 'u_idx',
-});
-db.Chatmessage.belongsTo(db.User, {
-  onDelete: 'cascade',
-  foreignKey: 'u_idx',
-});
-
-// Chattingroom : ChatMessage
-db.Chattingroom.hasMany(db.Chatmessage, {
-  foreignKey: 'r_idx',
-});
-db.Chatmessage.belongsTo(db.Chattingroom, {
-  onDelete: 'cascade',
-  foreignKey: 'r_idx',
-});
+// Relations 파일
+// require('./Relations')(db);
 
 
 module.exports = db;
