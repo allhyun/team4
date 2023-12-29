9ecbb1befc08cad680cdedba5d5606abba45ec0b
zyam1 <zyam8441@gmail.com>
2023-12-29 22:09:39 +0900

    feat:작성페이지 에디터 가져오기

--- FrontEnd/src/pages/Study/StudyMainPage.tsx
+++ FrontEnd/src/pages/Study/StudyMainPage.tsx
@@ -1,10 +1,10 @@
 //검색기능, 섬네일들,
 //css
 import '../../styles/study_main.scss';
-import StudyCreateBox from '../../components/StudyMainPage/StudyCreateBox';
+import StudyCreateBox from '../../components/Study/StudyCreateBox';
 
-import StudyThumbnailBox from '../../components/StudyMainPage/StudyThumbnailBox';
-import StudySearch from '../../components/StudyMainPage/StudySearch';
+import StudyThumbnailBox from '../../components/Study/StudyThumbnailBox';
+import StudySearch from '../../components/Study/StudySearch';
 
 const StudyMainPage = () => {
   return (
