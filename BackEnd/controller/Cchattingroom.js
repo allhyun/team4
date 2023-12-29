ef67262065b5083fba96a0f0177d42fd35d924d7
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-12-29 13:18:29 +0900

    ADD- express랑session설정 추가

--- BackEnd/controller/Cchattingroom.js
+++ BackEnd/controller/Cchattingroom.js
@@ -1,1 +1,9 @@
-const db = require("../model");
+const { Chattingroom, User } = require('../model');
+
+// exports.createChatRoom = (req, res) => {
+//     const data = {
+//         u_idx: req.session.user,
+//         r_name: req.body.r_name,
+
+//     }
+// }
