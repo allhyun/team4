7401dc6059e026540405cfde0f4410a3b8dd2d23
ErrorMonkey <puts.hello.world.return0@gmail.com>
2023-12-30 16:33:07 +0900

    feat: 로그인 페이지 - ㅎㅇ

---
+++ FrontEnd/src/pages/User/UserLoginPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserLoginPage = () => {
+  return <div>UserLoginPage</div>;
+};
+
+export default UserLoginPage;
---
+++ FrontEnd/src/pages/User/UserSignupPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserSignupPage = () => {
+  return <div>UserSignupPage</div>;
+};
+
+export default UserSignupPage;
