06e0095e3d8882d754b80781742729f8349a5ab1
zyam1 <zyam8441@gmail.com>
2024-01-02 00:34:47 +0900

    feat:디자인 조금 수정 스터디페이지 헤더 가져오기

--- FrontEnd/src/pages/Study/StudyMainPage.tsx
+++ FrontEnd/src/pages/Study/StudyMainPage.tsx
@@ -1,17 +1,20 @@
 //검색기능, 섬네일들,
 //css
-import '../../styles/study_main.scss';
-import StudyCreateBox from '../../components/Study/StudyCreateBox';
+import "../../styles/study_main.scss";
+import StudyCreateBox from "../../components/Study/StudyCreateBox";
 
-import StudyThumbnailBox from '../../components/Study/StudyThumbnailBox';
-import StudySearch from '../../components/Study/StudySearch';
+import StudyThumbnailBox from "../../components/Study/StudyThumbnailBox";
+import StudySearch from "../../components/Study/StudySearch";
+import StudyHeader from "../../components/Study/StudyHeader";
 
 const StudyMainPage = () => {
   return (
     <>
-      <StudySearch />
-      <StudyCreateBox />
-      <StudyThumbnailBox />
+      <StudyHeader />
+      <div className="st-main-container">
+        <StudyCreateBox />
+        <StudyThumbnailBox />
+      </div>
     </>
   );
 };
