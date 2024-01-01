055ed685c3af482fc9feefb1295f41de8b711c1b
nayounghye <yhna.dev@gmail.com>
2024-01-02 02:47:53 +0900

    Feat: marketeditor 가격창 기능 구현, 공통 container 블러적용 - 가격창 숫자정규식 적용 - 가격창 1억제한 적용 - 가격창 3번째 글자마다 콤마 적용

--- FrontEnd/src/components/Market/MarketEditor.tsx
+++ FrontEnd/src/components/Market/MarketEditor.tsx
@@ -12,6 +12,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { SassColor } from 'sass';
 
+// 데이터 타입
 interface DataType {
   category: string;
   location: string;
@@ -19,10 +20,19 @@ interface DataType {
   price: number | null;
   description: string;
   image: File | null;
+  textarea: string;
 }
 
 const MarketEditor: React.FC = () => {
+  // 글자수
+  const [titleLength, setTitleLength] = useState(0);
+  const [textareaLength, setTextareaLength] = useState(0);
+  // 리다이렉트용
   const navigate = useNavigate();
+  // 가격 형식
+  const [formattedPrice, setFormattedPrice] = useState('');
+
+  // 데이터 초기값
   const [DataType, setDataType] = useState<DataType>({
     category: '',
     location: '',
@@ -30,6 +40,7 @@ const MarketEditor: React.FC = () => {
     price: null,
     description: '',
     image: null,
+    textarea: '',
   });
 
   const handleInputChange = (
@@ -54,8 +65,46 @@ const MarketEditor: React.FC = () => {
     // 여기에 게시글 제출 로직 추가
   };
 
+  // /market으로 리다이렉트
   const handleCancel = () => {
     navigate('/market');
+  };
+
+  // 제목창 이벤트핸들러
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newTitle = e.target.value;
+    setTitleLength(newTitle.length); // 타이틀 길이 업데이트
+    setDataType({ ...DataType, title: newTitle });
+  };
+
+  // 설명창 관련 ---------------------------------------------------------------
+
+  // 설명창 이벤트핸들러
+  const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const newDescription = e.target.value;
+    setTextareaLength(newDescription.length); // 설명 길이 업데이트
+    setDataType({ ...DataType, textarea: newDescription });
+  };
+
+  // 가격창 관련 ---------------------------------------------------------------
+
+  const onlyNumber = (value: string) => {
+    // 숫자만 허용
+    const numbersOnly = value.replace(/[^0-9]/g, '');
+    // 숫자를 Number 타입으로 변환하고, 1억 이하의 값으로 제한
+    const limitedValue = Math.min(Number(numbersOnly), 100000000);
+    // 숫자를 문자열로 변환하고, 정규식을 사용하여 3자리마다 쉼표 추가
+    return limitedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
+  // 가격창 입력 핸들러
+  const handlePriceChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const numbersOnly = e.target.value.replace(/[^0-9]/g, '');
+    const limitedValue = Math.min(Number(numbersOnly), 100000000);
+    setDataType({ ...DataType, price: limitedValue }); // 1억 이하로 제한
+    setFormattedPrice(
+      limitedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    ); // 숫자만 가능
   };
 
   return (
@@ -65,7 +114,7 @@ const MarketEditor: React.FC = () => {
           상품이미지
           <span style={{ color: '#fcbaba' }}>＊</span>
           <div className="img_container">
-            <input type="file" id="market-img" name="market-img" />
+            <input type="file" id="market-img" name="img" />
             <label htmlFor="market-img">
               <div className="market-img">
                 <BsImage />
@@ -80,10 +129,11 @@ const MarketEditor: React.FC = () => {
             <input
               type="text"
               id="market-title"
-              name="market-title"
+              name="title"
               maxLength={40}
+              onChange={handleTitleChange}
             />
-            <span className="length">0/40</span>
+            <span className="title-length">{titleLength}/40</span>
           </div>
         </section>
         <section className="market-category">
@@ -95,7 +145,7 @@ const MarketEditor: React.FC = () => {
         <section className="market-region">
           거래지역<span style={{ color: '#fcbaba' }}>＊</span>
           <div className="region_container">
-            <input type="text" id="market-region" name="market-region" />
+            <input type="text" id="market-region" name="region" />
           </div>
         </section>
         <section className="market-price">
@@ -103,7 +153,13 @@ const MarketEditor: React.FC = () => {
           <div className="price_container">
             <label htmlFor="market-price">
               <span>₩</span>
-              <input type="text" id="market-price" name="market-price" />
+              <input
+                type="text"
+                id="market-price"
+                name="price"
+                value={formattedPrice}
+                onChange={handlePriceChange}
+              />
             </label>
           </div>
         </section>
@@ -112,15 +168,23 @@ const MarketEditor: React.FC = () => {
           <div className="textarea_container">
             <textarea
               id="market-textarea"
-              name="market-textarea"
+              name="textarea"
               maxLength={1000}
+              onChange={handleTextareaChange}
             />
+            <div className="textarea-length">{textareaLength}/1000</div>
           </div>
         </section>
-        <button type="button" onClick={handleCancel}>
+        <button
+          className="marketeditor-cancle"
+          type="button"
+          onClick={handleCancel}
+        >
           취소
         </button>
-        <button type="submit">등록</button>
+        <button className="marketeditor-regi" type="submit">
+          등록
+        </button>
       </form>
     </div>
   );
