b525d371bec42513c25ae99788cbc9ca06c69aa5
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-01-04 02:24:58 +0900

    feat: 파이어베이스 로그인 추가 - 파이어페이스 추가 - 팝업 cors 에러 해결 중

---
+++ FrontEnd/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBKaAOoJalTuxyfLuS8YBzlxsX5NBN4I1o',
+  authDomain: 'team4-dev-join.firebaseapp.com',
+  projectId: 'team4-dev-join',
+  storageBucket: 'team4-dev-join.appspot.com',
+  messagingSenderId: '918721406315',
+  appId: '1:918721406315:web:8e50c839e20fdaef34182e',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
