ef385edfebebccbac7b1f7a0969d8a05ee0f7513
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-01-04 15:13:20 +0900

    feat: userSlice 추가

--- FrontEnd/src/store/index.tsx
+++ FrontEnd/src/store/index.tsx
@@ -1,9 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import studySlice from "./modifyReducer";
+import { configureStore } from '@reduxjs/toolkit';
+import studySlice from './modifyReducer';
+import userSlice from './user.slice';
 
 const store = configureStore({
   reducer: {
     study: studySlice,
+    user: userSlice,
   },
 });
 
