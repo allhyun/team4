c75d0cb1841d98dde067f9c17a222c1c8b424737
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-01-07 23:26:32 +0900

    Feat: 포탈로 유저 메뉴 구현

--- FrontEnd/src/components/User/HeaderUser.tsx
+++ FrontEnd/src/components/User/HeaderUser.tsx
@@ -1,33 +1,47 @@
-// import React, { useEffect, useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { Link, useLocation } from 'react-router-dom';
-// import '../../styles/style.scss';
-// import { removeUserInfo } from '../../store/user.slice';
-// import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
+import '../../styles/style.scss';
+import { removeUserInfo } from '../../store/user.slice';
+import axios from 'axios';
+import UserMenu from './UserMenu';
 
-// const HeaderUser = () => {
+const HeaderUser = () => {
+  const [userMenuClicked, setUserMenuClicked] = useState<boolean>(false);
 
-//   return (
-//     <div>
-//       {loginUser.u_idx === null ? (
-//         <Link to="/login">로그인</Link>
-//       ) : (
-//         <div className="logined-user" onClick={userMenuHandler}>
-//           <div>{`${loginUser.nickname}`}</div>
-//           <img src={`http://localhost:8000/${loginUser.u_img}`} alt="" />
-//         </div>
-//       )}
-//       {userMenuClicked && (
-//         <div className="user-menu">
-//           <Link to="/">관심 목록</Link>
-//           <Link to="/">마이페이지</Link>
-//           <Link to="/" onClick={logoutHandler}>
-//             로그아웃
-//           </Link>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
+  // 로그인 로그아웃에 따른 상태 변화
+  const loginUser: any = useSelector((state: any) => state.user.user);
+  const dispatch = useDispatch();
 
-// export default HeaderUser;
+  const logoutHandler = () => {
+    setUserMenuClicked(false);
+    dispatch(removeUserInfo());
+    // 다른 리덕스 상태들도 삭제하는 dispatch도 추가해야 함
+    axios
+      .get('http://localhost:8000/user/logout', {
+        withCredentials: true,
+      })
+      .then((res) => console.log(res));
+  };
+
+  const userMenuHandler = () => {
+    setUserMenuClicked(!userMenuClicked);
+    console.log('click');
+  };
+
+  return (
+    <div>
+      {loginUser.u_idx === null ? (
+        <Link to="/login">로그인</Link>
+      ) : (
+        <div className="logined-user" onClick={userMenuHandler}>
+          <div>{`${loginUser.nickname}`}</div>
+          <img src={`http://localhost:8000/${loginUser.u_img}`} alt="" />
+        </div>
+      )}
+      {userMenuClicked && <UserMenu userMenuClicked={userMenuClicked} />}
+    </div>
+  );
+};
+
+export default HeaderUser;
--- FrontEnd/src/components/User/UserMenu.tsx
+++ FrontEnd/src/components/User/UserMenu.tsx
@@ -1,40 +1,24 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-// const UserMenu = () => {
-//   const [userMenuClicked, setUserMenuClicked] = useState(false);
+type Props = {
+  userMenuClicked: boolean;
+};
 
-//   // 로그인 로그아웃에 따른 상태 변화
-//   const loginUser: any = useSelector((state: any) => state.user.user);
-//   const dispatch = useDispatch();
+const UserMenu = ({ userMenuClicked }: Props) => {
+  return ReactDOM.createPortal(
+    <>
+      {userMenuClicked && (
+        <div className="user-menu">
+          <Link to="/">관심 목록</Link>
+          <Link to="/">마이페이지</Link>
+          <Link to="/">로그아웃</Link>
+        </div>
+      )}
+    </>,
+    document.getElementById('portal') as HTMLElement
+  );
+};
 
-//   const logoutHandler = () => {
-//     setUserMenuClicked(false);
-//     dispatch(removeUserInfo());
-//     // 다른 리덕스 상태들도 삭제하는 dispatch도 추가해야 함
-//     axios
-//       .get('http://localhost:8000/user/logout', {
-//         withCredentials: true,
-//       })
-//       .then((res) => console.log(res));
-//   };
-
-//   const userMenuHandler = () => {
-//     setUserMenuClicked(!userMenuClicked);
-//     console.log('click');
-//   };
-
-//   return ReactDOM.createPortal(
-//     <div className="user-menu">
-//       <Link to="/">관심 목록</Link>
-//       <Link to="/">마이페이지</Link>
-//       <Link to="/" onClick={logoutHandler}>
-//         로그아웃
-//       </Link>
-//     </div>
-//     // document.getElementById('portal')
-//   );
-// };
-
-// export default UserMenu;
+export default UserMenu;
